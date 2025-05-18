396fd3d49415912d50469437b16a25e1c170c95d
Chetan Chauhan <devilmafia71@gmail.com>
2025-05-18 22:10:43 +0530

    username changes

--- server/app.js
+++ server/app.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 // const cors = require('cors');
 
 const app = express();
@@ -6,18 +6,68 @@ const PORT = 5000;
 
 // app.use(cors());
 
-app.get('/api/users', (req, res) => {
+app.get("/api/users", (req, res) => {
   res.json([
-    { id: 1, name: 'Alice Smith', email: 'alice@example.com', phone: '123-456-7890' },
-    { id: 2, name: 'Bob Johnson', email: 'bob@example.com', phone: '987-654-3210' },
-    { id: 2, name: 'Bob Johnson', email: 'bob@example.com', phone: '987-654-3210' },
-    { id: 2, name: 'Bob Johnson', email: 'bob@example.com', phone: '987-654-3210' },
-    { id: 2, name: 'Bob Johnson', email: 'bob@example.com', phone: '987-654-3210' },
-    { id: 2, name: 'Bob Johnson', email: 'bob@example.com', phone: '987-654-3210' },
-    { id: 2, name: 'Bob Johnson', email: 'bob@example.com', phone: '987-654-3210' },
-    { id: 2, name: 'Bob Johnson', email: 'bob@example.com', phone: '987-654-3210' },
-    { id: 2, name: 'Bob Johnson', email: 'bob@example.com', phone: '987-654-3210' },
-    { id: 2, name: 'Bob Johnson', email: 'bob@example.com', phone: '987-654-3210' }
+    {
+      id: 1,
+      name: "Alice Smith",
+      email: "alice@example.com",
+      phone: "123-456-7890",
+    },
+    {
+      id: 2,
+      name: "Bob Johnson",
+      email: "bob@example.com",
+      phone: "987-654-3210",
+    },
+    {
+      id: 3,
+      name: "Bob Johnson",
+      email: "bob@example.com",
+      phone: "987-654-3210",
+    },
+    {
+      id: 4,
+      name: "Bob Johnson",
+      email: "bob@example.com",
+      phone: "987-654-3210",
+    },
+    {
+      id: 5,
+      name: "Bob Johnson",
+      email: "bob@example.com",
+      phone: "987-654-3210",
+    },
+    {
+      id: 6,
+      name: "Bob Johnson",
+      email: "bob@example.com",
+      phone: "987-654-3210",
+    },
+    {
+      id: 7,
+      name: "Bob Johnson",
+      email: "bob@example.com",
+      phone: "987-654-3210",
+    },
+    {
+      id: 8,
+      name: "Bob Johon",
+      email: "bob@example.com",
+      phone: "987-654-3210",
+    },
+    {
+      id: 9,
+      name: "Bob",
+      email: "bob@example.com",
+      phone: "987-654-3210",
+    },
+    {
+      id: 10,
+      name: "Johnson",
+      email: "bob@example.com",
+      phone: "987-654-3210",
+    },
   ]);
 });
 
