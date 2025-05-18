f2d4b04a898ceac185dfa464b265f9a80363604c
Chetan Chauhan <devilmafia71@gmail.com>
2025-05-18 22:11:47 +0530

    activated Cors

--- server/app.js
+++ server/app.js
@@ -1,10 +1,10 @@
 const express = require("express");
-// const cors = require('cors');
+const cors = require('cors');
 
 const app = express();
 const PORT = 5000;
 
-// app.use(cors());
+app.use(cors());
 
 app.get("/api/users", (req, res) => {
   res.json([
